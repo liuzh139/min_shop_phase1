6118bed757a15a0b559043ab71bcf588dbc6351d
liuzh139 <liuzh139@sb303-16.otago.ac.nz>
2013-08-21 16:12:00 +1200

    Created project. Create domain package, create Product class. Create gui package. Create MainMenu frame in gui, implement design(not include action). Create ProductDialog in gui, implement design(not include action). Create ProductReportDialog in gui, implement design(not include action). Create dao package. Create interface ProductDAO. Create ProductCollectionDAO implements ProductDAO. Copied JdbcConnection class.

---
+++ src/dao/JdbcConnection.java
@@ -0,0 +1,26 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.h2.jdbcx.JdbcConnectionPool;
+
+/**
+ *
+ * @author liuzh139
+ */
+public class JdbcConnection {
+
+   private static final String url = "jdbc:h2:tcp://localhost/labs/students";
+   private static final String username = "sa";
+   private static final String password = "";
+
+   private static JdbcConnectionPool pool = JdbcConnectionPool.create(url, username, password);
+
+   public static Connection getConnection() {
+      try {
+         return pool.getConnection();
+      } catch (SQLException ex) {
+         throw new RuntimeException(ex);
+      }
+   }
+}
