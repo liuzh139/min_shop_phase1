16bae441253c12f8fc6b8de062a415593a82ae78
Daisy Zhenyu Liu <bbc.abc@live.com>
2015-09-14 00:23:16 +1200

    Created SQL file for database creation on H2.

--- info221-phase1/ProductsSchema.sql
+++ info221-phase1/ProductsSchema.sql
@@ -7,3 +7,13 @@ price number not null,
 stock number not null,
 CONSTRAINT PK_products PRIMARY KEY(ID)
 );
+
+create table customer (
+username varchar2 not null,
+name varchar2 not null,
+address varchar2 not null,
+cardDetails varchar2 not null,
+password varchar2 not null,
+--
+constraint pk_customer primary key(username)
+);
